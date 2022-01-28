13f02797053bfd1a8937b9f23d8b594ef45a9b0a
Pauline Bourgeais <bourgeais.pauline@gmail.com>
2022-01-28 19:49:52 +0100

    add comments

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,14 +1,21 @@
-// Permet d'importer jsonwebtoken
+/* MIDDLEWARE D'AUTHENTIFICATION */
+
+// import le package qui vérifie les tokens
 const jwt = require('jsonwebtoken');
 
 // Permet de vérifier le token envoyé par le frontend
 module.exports = (req, res, next) => {
     try {
+        // récupération du token dans le header de la requête, ça nous retourne un tableau et on récupère le deuxième élément du tableau (le 1)
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+        // décoder le token
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        // récupération de l'user Id dans le décodage du token (qui est un objet js)
         const userId = decodedToken.userId;
+        // si on a un user Id dans le body de la requête et qu'il est différent de l'user id du token
         if (req.body.userId && req.body.userId !== userId) {
             throw 'User ID non valable';
+        // si l'user Id du body correspond à celui du token, on passe au prochain middleware
         } else {            
             next();
         }
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,26 +1,32 @@
-// Permet d'importer multer
+/* MIDDLEWARE MULTER */
+
+// import de multer
 const multer = require('multer');
 
-// Permet de définir l'extension des fichiers images
+// dictionnaire pour définir les extensions des fichiers
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
 };
 
-// Permet de configurer multer
+// diskstorage = méthode pour configurer le chemin et le nom des fichiers entrants
 const storage = multer.diskStorage({
-    // Permet de stocker les images dans le dossier images
+    // destination = fonction qui dit à multer où sera enregistré les fichiers
     destination: (req, file, callback) => {
+        //  deux arguments : null = pas d'erreurs, images = le nom du dossier de destination
         callback(null, 'images')
     },
-    // Permet de générer un nouveau nom de fichier image
+    // filename = explique à multer quel nom de fichier utilisé, pour ne pas avoir deux fois le même nom de fichier par exemple
     filename: (req, file, callback) => {
+        // génération du nouveau nom, split et join permette de remplacer les espaces par des _ pour ne pas avoir d'erreurs dans le serveur
         const name = file.originalname.split(' ').join('_');
+        // applique une extension aux fichiers
         const extension = MIME_TYPES[file.mimetype];
+        // création du nom final du fichier
         callback(null, name + Date.now() + '.' + extension);
     }
 });
 
-// Permet d'exporter le middleware multer
+// export du middleware
 module.exports = multer({storage}).single('image');
